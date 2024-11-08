f8a15e86aece5d2dfa915895efdd8ff3198aa17e
Devin J. Cornell <devincornell@github.com>
2024-11-08 12:20:18 -0500

    created new arg object for storing args. Can be passed between conversion function calls

--- pymddoc/compile.py
+++ pymddoc/compile.py
@@ -9,32 +9,79 @@ import tempfile
 
 from .util import val_or_None, map_or_None
 
+InputFormat = typing.Literal['md', 'markdown']
+RenderFormat = typing.Literal['html', 'pdf', 'docx']
+
+@dataclasses.dataclass
+class PandocArgs:
+    '''Dataclass that contains arguments for pandoc conversion.
+        See this page for more about pandoc markdown:
+            https://quarto.org/docs/authoring/markdown-basics.html
+    Args:
+        input_text: the markdown text to convert.
+        input_format: the format of the input file.
+        output_format: the format of the output file.
+        standalone: adds the --standalone flag to the pandoc command.
+        embed_resources: adds the --embed-resources flag to the pandoc command.
+        toc: adds the --toc flag to the pandoc command.
+        citeproc_bibliography: adds the --citeproc and --bibliography {fname}
+            arguments to the pandoc command.
+        template: adds the --template={fname} argument to the pandoc command.
+        extra_args: additional arguments to add to the pandoc command.
+        **kwargs: passed to pandoc.
+    '''
+    standalone: bool = False
+    embed_resources: bool = False
+    toc: bool = False
+    citeproc_bibliography: Path | None = None
+    template: Path | None = None
+    extra_args: list[str] | None = None
+    pandoc_kwargs: dict[str,typing.Any] | None = None
+
+    def to_list(self) -> tuple[list[str], dict[str,typing.Any]]:
+        '''Accept specific args to create list of arguments for the pandoc conversion.
+        Example: pandoc ... --standalone --embed-resources -f markdown --toc --citeproc --bibliography {filename}
+        '''
+        extra_args: list[str] = val_or_None(self.extra_args, [])
+
+        if self.template is not None:
+            extra_args.append(f'--template={self.template}')
+
+        if self.standalone:
+            extra_args.append('--standalone')
+        
+        if self.embed_resources:
+            extra_args.append('--embed-resources')
+
+        if self.toc:
+            extra_args.append('--toc')
+
+        if self.citeproc_bibliography is not None:
+            extra_args += ['--citeproc', f'--bibliography {self.citeproc_bibliography}']
+
+        return extra_args, self.pandoc_kwargs
+
+
 
 ###################### Converting to Other Formats with Pandoc ######################
-def convert_text(
+def pandoc_convert_text(
     input_text: str,
-    input_format: str,
+    input_format: typing.Literal['md', 'markdown'],
     output_format: typing.Literal['html'],
-    standalone: bool = False,
-    embed_resources: bool = False,
-    toc: bool = False,
-    citeproc_bibliography: typing.Optional[Path] = None,
-    template: typing.Optional[Path] = None,
-    extra_args: typing.Optional[list[str]] = None,
-    **kwargs
+    pandoc_args: PandocArgs | None = None,
 ) -> str:
     '''Convert the markdown file to another template.
         See this page for more about pandoc markdown:
             https://quarto.org/docs/authoring/markdown-basics.html
+    Args:
+        input_text: the markdown text to convert.
+        input_format: the format of the input file.
+        output_format: the format of the output file.
+        args: the arguments for the pandoc conversion.
     '''
-    extra_args = parse_args(
-        standalone=standalone,
-        embed_resources=embed_resources,
-        toc=toc,
-        citeproc_bibliography=citeproc_bibliography,
-        template=template,
-        extra_args=extra_args,
-    )
+    pandoc_args = val_or_None(pandoc_args, PandocArgs())
+    extra_args, kwargs = pandoc_args.to_list()
+
     return pypandoc.convert_text(
         source=input_text, 
         to=output_format,
@@ -43,34 +90,28 @@ def convert_text(
         **kwargs
     )
 
-def convert_file(
+def pandoc_convert_file(
     input_path: Path,
     input_format: str,
     output_path: Path,
     output_format: typing.Optional[typing.Literal['html', 'pdf', 'docx']] = None,
-    standalone: bool = False,
-    embed_resources: bool = False,
-    toc: bool = False,
-    citeproc_bibliography: typing.Optional[Path] = None,
-    template: typing.Optional[Path] = None,
-    extra_args: typing.Optional[list[str]] = None,
-    **kwargs
+    pandoc_args: PandocArgs | None = None,
 ) -> str | bytes:
     '''Convert the markdown text to a file using pandoc.
         See this page for more about pandoc markdown:
             https://quarto.org/docs/authoring/markdown-basics.html
+    Args:
+        input_path: path to the input markdown file.
+        input_format: the format of the input file.
+        output_path: path to the output file.
+        output_format: the format of the output file.
+        pandoc_args: the arguments for the pandoc conversion.
     '''
     input_path = map_or_None(input_path, Path)
     output_path = map_or_None(output_path, Path)
-
-    extra_args = parse_args(
-        standalone=standalone,
-        embed_resources=embed_resources,
-        toc=toc,
-        citeproc_bibliography=citeproc_bibliography,
-        template=template,
-        extra_args=extra_args,
-    )
+    
+    pandoc_args = val_or_None(pandoc_args, PandocArgs())
+    extra_args, kwargs = pandoc_args.to_list()
 
     return pypandoc.convert_file(
         source_file=str(input_path), 
@@ -81,43 +122,3 @@ def convert_file(
         **kwargs,
     )
 
-def parse_args(
-    standalone: bool = False,
-    embed_resources: bool = False,
-    toc: bool = False,
-    citeproc_bibliography: typing.Optional[Path] = None,
-    template: typing.Optional[Path] = None,
-    extra_args: typing.Optional[list[str]] = None,
-) -> list[str]:
-    '''Accept specific args to create list of arguments for the pandoc conversion.
-    Example: pandoc ... --standalone --embed-resources -f markdown --toc --citeproc --bibliography {filename}
-    Args:
-        standalone: bool = False
-            If true, the output will be a standalone document.
-        embed_resources: bool = False
-            If true, resources will be embedded in the output.
-        toc: bool = False
-            If true, a table of contents will be generated.
-        citeproc_bibliography: typing.Optional[Path] = None
-            If not None, the path to a bibliography file.
-        template: If not None, the path to a template file.
-        extra_args: typing.Optional[list[str]] = None
-    '''
-    extra_args: list[str] = val_or_None(extra_args, [])
-
-    if template is not None:
-        extra_args.append(f'--template={template}')
-
-    if standalone:
-        extra_args.append('--standalone')
-    
-    if embed_resources:
-        extra_args.append('--embed-resources')
-
-    if toc:
-        extra_args.append('--toc')
-
-    if citeproc_bibliography is not None:
-        extra_args += ['--citeproc', f'--bibliography {citeproc_bibliography}']
-
-    return extra_args
